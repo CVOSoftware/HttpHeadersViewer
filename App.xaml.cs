b37afd0aca9e06801a9091a8a7b4086d1706756c
cvosoftware <cvosoftware@gmail.com>
2019-08-20 22:55:52 +0300

    overrided start point

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,18 @@
 ﻿using System.Windows;
+using HttpHeadersViewer.View;
+using HttpHeadersViewer.ViewModel;
 
 namespace HttpHeadersViewer
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainViewModel viewModel = new MainViewModel();
+            MainWindow view = new MainWindow();
+            view.DataContext = viewModel;
+            view.Show();
+        }
     }
 }
