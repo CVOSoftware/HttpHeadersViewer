66b78366c3cd7dddd58917c04e4e30026cb2d120
cvosoftware <cvosoftware@gmail.com>
2019-08-20 22:49:39 +0300

    added base classes

---
+++ ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace HttpHeadersViewer.ViewModel.Base
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
